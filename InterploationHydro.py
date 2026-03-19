cf4f06398c9e287f15a05c843d908ee744c5ae2a
mariaborgesb <maruborges@hotmail.com>
2026-03-19 13:12:53 +1300

    Update InterploationHydro.py

--- InterploationHydro.py
+++ InterploationHydro.py
@@ -1,55 +1,68 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 18 16:17:00 2024
-
-@author: haddadchia
-"""
-
 # %%
-def interpolhydro (fromArrayList,toArray, unitflowdict, depthdict, Qdict, xpoints_dict):
-    # Linear interpolation of hydrological data
-    import os
-    import pandas as pd
+def interpolhydro(fromArrayList, toArray, unitflowdict, depthdict, Qdict, xpoints_dict):
     import numpy as np
-    from datetime import datetime
     import scipy
-    qinterp_dict={}     # empty disctionary for interpolated unitflow
-    depthinterp_dict={} # empty dictionary for interpolated depth
-    Qinterp_dict={} # empty dictionary for interpolated flow
-    segment=0
-    for n in fromArrayList:
-        if n!=fromArrayList[-1]:
-            print(n)
-            #print(segment)
-            q_US=np.array(unitflowdict[n])
-            q_DS=np.array(unitflowdict[toArray[segment]])
-            depth_US=np.array(depthdict[n])
-            depth_DS=np.array(depthdict[toArray[segment]])
-            Q_US=np.array(Qdict[n])
-            Q_DS=np.array(Qdict[toArray[segment]])
-            qinterp=np.zeros((len(q_US),xpoints_dict[n]+2))
-            qinterp_struct=scipy.interpolate.interp1d([1,xpoints_dict[n]+2], np.vstack([q_US,q_DS]),kind='linear',axis=0)
-            depthinterp=np.zeros((len(depth_US),xpoints_dict[n]+2))
-            depthinterp_struct=scipy.interpolate.interp1d([1,xpoints_dict[n]+2], np.vstack([depth_US,depth_DS]),kind='linear',axis=0)
-            Qinterp=np.zeros((len(Q_US),xpoints_dict[n]+2))
-            Qinterp_struct=scipy.interpolate.interp1d([1,xpoints_dict[n]+2], np.vstack([Q_US,Q_DS]),kind='linear',axis=0)
-            for i in range(xpoints_dict[n]+2):
-                #depthinterp[:,i]=depthinterp_struct(i+1)
-                qinterp[:,i]=qinterp_struct(i+1)
-                depthinterp[:,i]=depthinterp_struct(i+1)
-                Qinterp[:,i]=Qinterp_struct(i+1)
-            qinterpt=np.transpose(qinterp)
-            qinterp_dict[n]=qinterpt  # filled dictionary for interpolated unitflow
-            depthinterpt=np.transpose(depthinterp)
-            depthinterp_dict[n]=depthinterpt # filled dictionary for interpolated depth
-            Qinterpt=np.transpose(Qinterp)
-            Qinterp_dict[n]=Qinterpt # filled dictionary for interpolated flow
-            segment=segment+1
-        else:
-            print ('Sink!',n)
-    interp_out=pd.DataFrame({'depth_interp':depthinterp_dict,'q_interp':qinterp_dict}) # output of interpolations as data frame
+    import pandas as pd
 
+    qinterp_dict = {}      # interpolated unitflow
+    depthinterp_dict = {}  # interpolated depth
+    Qinterp_dict = {}      # interpolated discharge
+
+    segment = 0
+    n_to = len(toArray)  
+
+    for n in fromArrayList:
+        q_US     = np.array(unitflowdict[n])
+        depth_US = np.array(depthdict[n])
+        Q_US     = np.array(Qdict[n])
+
+        ds_to_node = toArray[segment] if segment < n_to else None
+
+        ds_from_node = None
+        if ds_to_node is not None:
+            try:
+                idx = fromArrayList.index(ds_to_node)  # find the reach that starts at this TO_NODE
+                ds_from_node = fromArrayList[idx]
+            except ValueError:
+                ds_from_node = None  # no downstream reach => it is a sink
+
+        if (ds_from_node is not None) and (ds_from_node in unitflowdict) and (ds_from_node in depthdict) and (ds_from_node in Qdict):
+            q_DS     = np.array(unitflowdict[ds_from_node])
+            depth_DS = np.array(depthdict[ds_from_node])
+            Q_DS     = np.array(Qdict[ds_from_node])
+        else:
+            q_DS     = q_US.copy()
+            depth_DS = depth_US.copy()
+            Q_DS     = Q_US.copy()
+
+        cols = xpoints_dict[n] + 2  
+        qinterp     = np.zeros((len(q_US),     cols))
+        depthinterp = np.zeros((len(depth_US), cols))
+        Qinterp     = np.zeros((len(Q_US),     cols))
+
+        q_stack     = np.vstack([q_US,     q_DS]) 
+        depth_stack = np.vstack([depth_US, depth_DS])
+        Q_stack     = np.vstack([Q_US,     Q_DS])    
+
+        qinterp_struct     = scipy.interpolate.interp1d([1, cols], q_stack,     kind='linear', axis=0, assume_sorted=True)
+        depthinterp_struct = scipy.interpolate.interp1d([1, cols], depth_stack, kind='linear', axis=0, assume_sorted=True)
+        Qinterp_struct     = scipy.interpolate.interp1d([1, cols], Q_stack,     kind='linear', axis=0, assume_sorted=True)
+
+        for i in range(cols):
+            pos = i + 1
+            qinterp[:, i]     = qinterp_struct(pos)
+            depthinterp[:, i] = depthinterp_struct(pos)
+            Qinterp[:, i]     = Qinterp_struct(pos)
+
+        qinterpt     = np.transpose(qinterp)
+        depthinterpt = np.transpose(depthinterp)
+        Qinterpt     = np.transpose(Qinterp)
+
+        qinterp_dict[n]     = qinterpt
+        depthinterp_dict[n] = depthinterpt
+        Qinterp_dict[n]     = Qinterpt
+
+        segment += 1
+    
+    interp_out = pd.DataFrame({'depth_interp': depthinterp_dict, 'q_interp': qinterp_dict})
     return (qinterp_dict, depthinterp_dict, Qinterp_dict, interp_out)
-    
-    
-    
